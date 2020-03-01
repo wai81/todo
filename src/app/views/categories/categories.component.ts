d074065fd48508b60f39a3698a0bc26f1eb8762d
wai81 <vashkelevich.andrei@gmail.com>
2020-03-01 22:22:35 +0300

    add checkbox check categories

--- src/app/views/categories/categories.component.ts
+++ src/app/views/categories/categories.component.ts
@@ -9,6 +9,7 @@ import {Category} from '../../model/Category';
 })
 export class CategoriesComponent implements OnInit {
   categories: Category[];
+  selectedCategory: Category;
 
   constructor(private dataHandler: DataHandlerService) {
   }
@@ -19,6 +20,7 @@ export class CategoriesComponent implements OnInit {
   }
 
   showTasksByCategory(category: Category) {
+    this.selectedCategory = category;
     this.dataHandler.fillTasksByCategory(category);
   }
 }
